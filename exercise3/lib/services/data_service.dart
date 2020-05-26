1d2cdada1c2845cbbe1d325aaed603fc98562367
HafiziZulkeflee <62562921+HafiziZulkeflee@users.noreply.github.com>
2020-05-26 17:20:22 +0800

    Update data_service.dart

--- exercise3/lib/services/data_service.dart
+++ exercise3/lib/services/data_service.dart
@@ -3,11 +3,10 @@
 // Semester 2, 2019/2020
 // Exercise 3: HTTP and JSON
 //
-// Name 1:  KEVIN
-// Name 2:  HAFIZI
+// Name 1:  KEVIN THIEN YOU HENG
+// Name 2:  MUHAMMAD HAFIZI BIN MD ZULKEFLEE
 //-----------------------------------------------------------
 import 'dart:convert';
-//import 'package:rest_client/rest_client.http' as http;
 import 'package:http/http.dart' as http;
 import '../models/todo.dart';
 
@@ -29,7 +28,7 @@ class DataService {
 
   // TODO 2: Complete this method. It is an helper for the HTTP POST request
   Future post(String endpoint, {dynamic data}) async {
-     final response = await http.post('$baseUrl/$endpoint',
+     final http.Response response = await http.post('$baseUrl/$endpoint',
         headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
 
     if (response.statusCode == 200) {
@@ -53,7 +52,6 @@ class DataService {
   Future delete(String endpoint) async {
     final response = await http.delete('$baseUrl/$endpoint',
         headers: {'Content-Type': 'application/json'});
-
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     }
@@ -62,54 +60,25 @@ class DataService {
 
   // TODO 5: Complete this method. It is meant for getting the list of TODO s from the server
   Future<List<Todo>> getTodoList() async {
-   List<Todo> todoList = [];
-
-    Response response = await TodoUtils.getTodoList();
-    print("Title is ${response.statusCode}");
-    print("Description is ${response.body}");
-
-    if (response.statusCode == 200) {
-      var body = json.decode(response.body);
-      var results = body["results"];
-
-      for (var todo in results) {
-        todoList.add(Todo.fromJson(todo));
-      }
-
-    } else {
-      //Handle error
-    }
-
-    return todoList;
+    final json = await get('todos');
+    return (json as List).map((item) => Todo.fromJson(item)).toList();
   }
-  
 
   // TODO 6: Complete this method. It is meant for updating the status of a given TODO  (whether is completed or not) in the server
   Future<Todo> updateTodoStatus({int id, bool status}) async {
-    final json = await patch('todo/$id/$status');
-    json['title'] = await patch(
-        Todo,where "id=?", where status "status=?", whereArgs: [todo.id] ); 
-
+    final json = await patch('todos/$id',data:{'completed':status});
     return Todo.fromJson(json);
   }
 
   // TODO 7: Complete this method. It is meant for creating a new TODO  in the server
   Future<Todo> createTodo({Todo todo}) async {
-     final json = await post('todos/$todo');
-    json['title'] = await post(
-        Todo, todo.toJson()
-    ); 
-
+    final json = await post('todos/',data:{'title':todo.title,'completed':todo.completed});
     return Todo.fromJson(json);
   }
 
   // TODO 8: Complete this method. It is meant for deleting a given TODO  from the server
   Future<Todo> deleteTodo({int id}) async {
     final json = await delete('todos/$id');
-    json['title'] = await delete(
-        Todo, where: 'id=?', whereArgs:[id]
-    ); 
-
     return Todo.fromJson(json);
   }
 }
